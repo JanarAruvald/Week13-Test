3c74d2e5b06aed14481c71a808017a7b76202456
Opilane TTHK <opilane@tthk.ee>
2021-11-23 16:25:58 +0200

    Week13-Test

---
+++ Week13-Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Week13_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string newPath = @"C:\Users\opilane\samples";
+
+            Console.WriteLine("Siseta kausta nimi:");
+            string KasutajaKaust = Console.ReadLine();
+
+            string newDirectory = @$"{newPath}\{KasutajaKaust}";
+            bool DirectoryExists = Directory.Exists(newDirectory);
+
+            if (DirectoryExists)
+            {
+                Console.WriteLine($"{KasutajaKaust} already exists in Samples.");
+            }
+            else
+            {
+                Directory.CreateDirectory(newDirectory);
+                Console.WriteLine($"Directory {KasutajaKaust} has been created.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ylesanne4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Ylesanne4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootdirect = $@"C:\Users\opilane\samples\Week13";
+            Console.WriteLine("What file would you like displayed?");
+            string userFile = Console.ReadLine();
+            string fileDirect = $@"{rootdirect}\{userFile}.txt";
+            bool exists = File.Exists(fileDirect);
+            if (exists)
+            {
+                string[] dataFromFile = File.ReadAllLines(fileDirect);
+                if (userFile == "toyota" || userFile == "bmw" || userFile == "tesla")
+                {
+                    foreach (string line in dataFromFile)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{userFile}.txt file does not exist.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{userFile}.txt file does not exist.");
+
+            }
+        }
+    }
+}
